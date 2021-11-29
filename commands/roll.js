bf7f1d7a02b421867fb288a593dd59cf9bbc8e20
Nick Conklin <nconkl1@outlook.com>
2021-11-29 09:26:26 -0500

    fixed an issue with roll where it would roll 0 and never 20

--- commands/roll.js
+++ commands/roll.js
@@ -1,14 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
 		.setDescription('Rolls a d20! Useful for rolling for loot!'),
 	async execute(interaction) {
-		await interaction.reply(`<@${interaction.member.id}> rolled a ${getRandomInt(20)}`);
+		await interaction.reply(`<@${interaction.member.id}> rolled a ${getRandomInt(1, 21)}`);
 	},
 };
